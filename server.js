91f522cb66c014697310e7460f1c2020de98694b
Emmanuel <emmayodayo@gmail.com>
2022-12-12 11:56:26 +0100

    added test stuff

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const {MONGO_URI} = require('./config');
+
 const app = express()
+
+mongoose.connect(MONGO_URI, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+})
+.then(() => console.log('MongoDB connected!'))
+.catch(err => console.log(err));
 
 app.get('/', (req, res) => {
 	res.send('hello from express')
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`server running at port ${PORT}`))
+app.listen(PORT, () => console.log(`server running at port ${PORT}`));
