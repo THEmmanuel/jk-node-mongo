28ebc17549d034755bc834e590e46fc84ff47164
Emmanuel <emmayodayo@gmail.com>
2022-12-13 10:45:25 +0100

    add body parser

--- server.js
+++ server.js
@@ -2,18 +2,20 @@ const express = require('express');
 const mongoose = require('mongoose');
 const {MONGO_URI} = require('./config');
 // const postsRoutes = require('./routes/posts_controller');
+const apiRoutes = require('./api-routes')
 
 const app = express();
 app.use(express.json());
 
-mongoose.connect(MONGO_URI, {
-	useNewUrlParser: true,
-	useUnifiedTopology: true,
-})
-.then(() => console.log('MongoDB connected!'))
-.catch(err => console.log(err));
+// mongoose.connect(MONGO_URI, {
+// 	useNewUrlParser: true,
+// 	useUnifiedTopology: true,
+// })
+// .then(() => console.log('MongoDB connected!'))
+// .catch(err => console.log(err));
 
 // app.use('/posts', postsRoutes);
+app.use('/api', apiRoutes)
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`server running at port ${PORT}`));
